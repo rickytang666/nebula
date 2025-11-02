4dd9686e3c6425dd44e9c01825129ac87ba6c1c9
Dongwan Seoh <dongwanseoh@gmail.com>
2025-11-02 14:52:52 -0500

    added basic bone files

--- Project/src/frontend/app/index.tsx
+++ Project/src/frontend/app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
 
-export default function Index() {
+export default function Intro() {
+  const router = useRouter();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Button title="Login" onPress={() => router.push("/login")} />
+      <Button title="Sign Up" onPress={() => router.push("/signup")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  title: { fontSize: 24, marginBottom: 20 },
+});
