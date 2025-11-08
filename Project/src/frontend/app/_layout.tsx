9bc0f35cae00b03be5de7454bb11b60685e41161
Dongwan Seoh <dongwanseoh@gmail.com>
2025-11-08 00:59:58 -0500

    added theme configurations

--- Project/src/frontend/app/_layout.tsx
+++ Project/src/frontend/app/_layout.tsx
@@ -2,10 +2,17 @@ import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
+    
+    <GluestackUIProvider mode="dark">
+      <SafeAreaProvider>
       <Stack screenOptions={{ headerShown: false }} />
     </SafeAreaProvider>
+    </GluestackUIProvider>
+  
   );
 }
